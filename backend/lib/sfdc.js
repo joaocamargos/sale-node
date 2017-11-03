cf4fe3b72e69c074773ca6612ac16b3b50897ce1
andrews <andrews.campos@hotmail.com>
2017-11-03 15:04:05 -0200

    add url

--- backend/lib/sfdc.js
+++ backend/lib/sfdc.js
@@ -77,7 +77,7 @@ class ServiceCloud {
 		if (!/^([a-zA-Z0-9]{15}|[a-zA-Z0-9]{18})$/g.test(id)) return cb(new Error('Invalid Salesforce-Id given.'));
 
 		const self = this;
-		const query = 'SELECT <INSERT NAME OF FIELD HERE> FROM <INSERT YOUR OBJECT HERE> WHERE Id = \'' + id + '\'';
+		const query = 'SELECT <INSERT N_loginAME OF FIELD HERE> FROM <INSERT YOUR OBJECT HERE> WHERE Id = \'' + id + '\'';
 
 		// Login if necessary
 		self._login((e) => {
